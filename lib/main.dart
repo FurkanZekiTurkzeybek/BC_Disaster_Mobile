e95898f58e96ced10b5bdb542a3592c4146deb3c
FurkanZekiTürkzeybek <furkan.fztr@gmail.com>
2023-03-27 13:38:53 +0300

    -Can read the hash from firebase and update the information

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart' as FB;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart';
 import 'package:web3dart/web3dart.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).whenComplete(() {
+    print("completed");
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -29,25 +36,31 @@ class MyHomePage extends StatefulWidget {
 
   final String title;
 
+  var db = FB.FirebaseFirestore.instance;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  FB.FirebaseFirestore getDB() {
+    return widget.db;
+  }
 
+  late var db = getDB();
 
   late Client httpClient;
   late Web3Client ethClient;
 
-  final String myAddress = ""; // Metamask wallet
-  final String bcURL = "https://goerli.infura.io/v3/1670a51d46d74984873f6a273c285335";
+  final String myAddress =
+      "0x1cEDc507F8478ECAc0fc6b710c8C039050AD0aa8"; // Metamask wallet
+  final String bcURL =
+      "https://sepolia.infura.io/v3/1670a51d46d74984873f6a273c285335";
 
   var name = "Name";
   var surname = "Surname";
   var ssn = "123456789";
   var address = "TR";
-
 
   @override
   void initState() {
@@ -57,12 +70,27 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
+  Future<String> getContractAddress() async {
+    final docRef = db.collection("users").doc("test");
+    String contAddress = "";
+    /*docRef.get().then((FB.DocumentSnapshot doc) {
+      final data = doc.data() as Map<String, dynamic>;
+      String addr = data.entries.first.value.toString();
+    });*/
+    //This part is discarded.
+
+    FB.DocumentSnapshot doc = await docRef.get();
+    final data = doc.data() as Map<String, dynamic>;
+    contAddress = data.entries.first.value.toString();
+    print("First print is: $contAddress");
+    return contAddress;
+  }
+
   Future<DeployedContract> getContract() async {
     String abiFile = await rootBundle.loadString("assets/contract.json");
 
-
-    String contractAddress = ""; // deployed contract address
-
+    String contractAddress =
+        await getContractAddress(); // deployed contract address
 
     final contract = DeployedContract(ContractAbi.fromJson(abiFile, "Person"),
         EthereumAddress.fromHex(contractAddress));
@@ -92,7 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> deploy() async {
     Credentials key = EthPrivateKey.fromHex(
-        ""); // metamask wallet private key
+        "5097a6d03dc87e60520a428433f4ad15aaab20d5aaba9e4188ccb82c7ad2196f"); // metamask wallet private key
 
     final contract = await getContract();
 
@@ -102,16 +130,12 @@ class _MyHomePageState extends State<MyHomePage> {
         key,
         Transaction.callContract(
             contract: contract, function: function, parameters: ["flutter"]),
-        chainId: 5);
-
+        chainId: 11155111);
 
     Future.delayed(const Duration(seconds: 20), () {
       getContractContents();
-
     });
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -128,7 +152,7 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(ssn),
             Text(address),
             ElevatedButton(
-                onPressed: (){
+                onPressed: () {
                   setState(() {
                     deploy();
                   });
