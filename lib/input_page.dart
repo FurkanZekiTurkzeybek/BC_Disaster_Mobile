70b546f26de5e994857abe64ea76b1e9fbef4ade
FurkanZekiTurkzeybek <furkan.fztr@gmail.com>
2023-05-10 02:54:21 +0300

    -Server connection final form

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -4,6 +4,8 @@ import 'package:dapp/main.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as HTTP;
+
 // void main() {
 //   runApp(MaterialApp(
 //     title: "randomTitle",
@@ -23,8 +25,8 @@ void main() async {
     home: InputPage(),
   ));
   var client = HttpClient();
-  var request = await client.getUrl(Uri.parse(
-      'https://www.example.com')); //server ile ilgili seyler calismadi
+  var request = await client.getUrl(
+      Uri.parse('https://10.0.2.2:3000')); //server ile ilgili seyler calismadi
   var response = await request.close();
   var responseBody = await response.transform(utf8.decoder).join();
   print(responseBody);
@@ -56,7 +58,7 @@ class _InputPageState extends State<InputPage> {
   var _address;
   var _ssn;
 
-  get http => http; //localhost:3000/api/person;
+  // get http => "https://10.0.2.2:3000"; //localhost:3000/api/person;
 
 //flutterdan alinan inputlar ile person objesi olusuyo. submite basinca da status screene yonlendiriyor.
   @override
@@ -103,49 +105,51 @@ class _InputPageState extends State<InputPage> {
               },
             ),
             SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                Person person = Person(
-                    name: _name,
-                    surname: _surname,
-                    address: _address,
-                    ssn: _ssn);
-                print('Name: ${person.name}');
-                print('Surname: ${person.surname}');
-                print('Address: ${person.address}');
-                print('SSN: ${person.ssn}');
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const MyApp()));
-              },
-              child: Text('Submit'),
-            ),
-
             // ElevatedButton(
-            //   onPressed: () async {
+            //   onPressed: () {
             //     Person person = Person(
             //         name: _name,
             //         surname: _surname,
             //         address: _address,
             //         ssn: _ssn);
-            //     final url = Uri.parse('http://localhost:3000/api/person');
-            //     final response = await http.post(
-            //       url,
-            //       headers: {'Content-Type': 'application/json'},
-            //       body: json.encode({
-            //         'name': person.name,
-            //         'surname': person.surname,
-            //         'address': person.address,
-            //         'ssn': person.ssn,
-            //       }),
-            //     );
-            //     if (response.statusCode == 200) {
-            //       print('Person saved successfully!');
-            //     } else {
-            //       print('Failed to save person: ${response.statusCode}');
-            //     }
+            //     print('Name: ${person.name}');
+            //     print('Surname: ${person.surname}');
+            //     print('Address: ${person.address}');
+            //     print('SSN: ${person.ssn}');
+            //     Navigator.push(context,
+            //         MaterialPageRoute(builder: (context) => const MyApp()));
             //   },
-            //   child: const Text('Submit'),
+            //   child: Text('Submit'),
             // ),
+
+            ElevatedButton(
+              onPressed: () async {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const MyApp()));
+                Person person = Person(
+                    name: _name,
+                    surname: _surname,
+                    address: _address,
+                    ssn: _ssn);
+                final url = Uri.parse('http://10.0.2.2:3000/api/person');
+                final response = await HTTP.post(
+                  url,
+                  headers: {'Content-Type': 'application/json'},
+                  body: json.encode({
+                    'name': person.name,
+                    'surname': person.surname,
+                    'address': person.address,
+                    'ssn': person.ssn,
+                  }),
+                );
+                if (response.statusCode == 200) {
+                  print('Person saved successfully!');
+                } else {
+                  print('Failed to save person: ${response.statusCode}');
+                }
+              },
+              child: const Text('Submit'),
+            ),
 
             // bu kisim server js ile ilgili
           ],
